6b393bd7176908ec3f42f5ab31b60b1667b53d09
AlbertoF17 <albertofergom@gmail.com>
2022-11-18 13:54:20 +0100

    first commit

---
+++ HTML Media - Alberto Fernandez/js/script.js
@@ -0,0 +1,10 @@
+
+function defaultVolume () {
+    var vid1 = document.getElementById("video1");
+    var vid2 = document.getElementById("video2");
+    vid1.volume = 0.5;
+    vid2.volume = 0.5;
+    
+    var au = document.getElementById("audio");
+    au.volume = 0.5;
+}
