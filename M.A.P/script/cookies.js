7b7ebe9f5d9dd159fb1511a3f58032e6e7137f49
AlbertoF17 <albertofergom17@gmail.com>
2022-12-09 15:46:42 +0100

    Marcas

--- M.A.P/script/cookies.js
+++ M.A.P/script/cookies.js
@@ -2,7 +2,7 @@ const botonAceptarCookies = document.getElementById("btn-aceptar-cookies");
 const avisoCookies = document.getElementById("aviso-cookies");
 const fondoAvisoCookies = document.getElementById("fondo-aviso-cookies");
 
-// CONDICIONAL USADO SÓLO PARA QUE PREGUNTE LAS COOKIES 1 VEZ POR EQUIPO QUE ENTRA A LA PÁGINA
+//CONDICIONAL USADO SÓLO PARA QUE PREGUNTE LAS COOKIES 1 VEZ POR EQUIPO QUE ENTRA A LA PÁGINA
 if (!localStorage.getItem("cookies-aceptadas")) {
     avisoCookies.classList.add("activo");
     fondoAvisoCookies.classList.add("activo");
