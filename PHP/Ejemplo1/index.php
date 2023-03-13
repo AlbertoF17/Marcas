c0e3356b3598ef021255e75bdd3f6173cb6365c6
AlbertoF17 <albertofergom17@gmail.com>
2023-03-13 19:34:14 +0100

    Marcas

--- PHP/Ejemplo1/index.php
+++ PHP/Ejemplo1/index.php
@@ -1,1 +1,28 @@
-<?php echo "hola" ?>
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="description" content="Aplicacion de lista de tareas">
+    <meta name="keywords" content="task, list, html, js, bootstrap">
+    <meta name="language" content="EN">
+    <meta name="author" content="joaquin.borrego@vedruna.es">
+    <meta name="robots" content="index,follow">
+    <meta name="revised" content="Tuesday, January 31th, 2023, 05:00pm">
+    <meta name="viewport" content="width=device-width, initial scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge, chrome1">
+    <title>Clase PHP</title>
+
+</head>
+<body>
+    <h1><?php echo "Hola"?></h1>
+    <h1><?php print("Hola")?></h1>
+    <h1><?= "Hola"?></h1>
+    $nombre = "Pablo";
+    $apellido = "López";
+    $nombreCompleto = "$nombre Lopez";
+    <h1><?= $nombreCompleto ?></h1>
+    /* COPIAR LA PAGINA Q SUBA joaquin */
+</body>
+</html>
