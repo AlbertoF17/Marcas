2ce4a7328d4efff3bdc87256bb156cbbd170d98b
AlbertoF17 <albertofergom@gmail.com>
2023-04-10 12:35:12 +0200

    Marcas

--- PHP/EjercicioRegistroLogin/connection/connection.php
+++ PHP/EjercicioRegistroLogin/connection/connection.php
@@ -3,12 +3,12 @@ function connection(){
     $host = "localhost:3306";
     $user = "root";
     $pass = "root";
-
     $bd = "socialpkmn";
-
+    
     $connect=mysqli_connect($host, $user, $pass);
-
     mysqli_select_db($connect, $bd);
+
+    session_start();
 
     return $connect;
 }
